e3eb7ccadad56271d69549f8e20e5e7c1c358f91
RenatoAmaral <srkimbo@gmail.com>
2018-03-18 22:49:36 +0000

    Task completed

--- index.js
+++ index.js
@@ -1,29 +1,93 @@
-let balance = 500.00;
+class Account {
 
-class Withdrawal {
+  constructor(username) {
+    this.username = username;
+    this.transactions = [];
+  }
 
-  constructor(amount) {
+  get balance() {
+    // Calculate the balance using the transaction objects.
+  let balance = 0;
+  this.transactions.forEach(function(transaction) {
+    balance += transaction.value;
+  })
+  return balance;
+  }
+
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
+  }
+}
+
+class Transaction {
+
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
+  }
+  get isAllowed(){
+    return this.account.balance + this.value > 0;
   }
 
   commit() {
-    balance -= this.amount;
+    if(this.isAllowed){
+      // Keep track of the time of the transaction
+      this.time = new Date();
+      // Add the transaction to the account
+      this.account.addTransaction(this);
+      return true;
+    }else{
+      return false;
+    }
   }
-
 }
 
+class Deposit extends Transaction {
+   // Update the balance in the account
+  get value() {
+    return this.amount;
+  }
+}
 
-
+class Withdrawal extends Transaction {
+  get value() {
+    return -this.amount;
+  }
+}
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
-t1.commit();
-console.log('Transaction 1:', t1);
+const myAccount = new Account("snow-patrol");
 
-t2 = new Withdrawal(9.99);
-t2.commit();
-console.log('Transaction 2:', t2);
+console.log("Name", myAccount.username, "Initial balance", myAccount.balance)
 
-console.log('Balance:', balance);
+t1 = new Withdrawal(50.25, myAccount);
+console.log('\nTransaction 1:', t1);
+let res1 = t1.commit();
+console.log('Commit result:', res1);
+console.log('Balance:', myAccount.balance);
+
+t2 = new Deposit(10, myAccount);
+console.log('\nTransaction 2:', t2);
+let res2 = t2.commit();
+console.log('Commit result:', res2);
+console.log('Balance:', myAccount.balance);
+
+t3 = new Withdrawal(9.99, myAccount);
+console.log('\nTransaction 3:', t3);
+let res3 = t3.commit();
+console.log('Commit result:', res3);
+console.log('Balance:', myAccount.balance);
+
+t4 = new Deposit(120.00, myAccount);
+console.log('\nTransaction 4:', t4);
+let res4 = t4.commit();
+console.log('Commit result:', res4);
+console.log('Balance:', myAccount.balance);
+
+t5 = new Deposit(1120.00, myAccount);
+console.log('\nTransaction 5:', t5);
+let res5 = t5.commit();
+console.log('Commit result:', res5);
+console.log('Balance:', myAccount.balance);
